41757fa883ed1c8b06473d8d7a238ed3caeb3f5b
xadvent <piercestrutt@gmail.com>
2023-06-05 21:20:51 -0600

    Clear works dynamically - stopped calc breaking

--- script.js
+++ script.js
@@ -8,20 +8,19 @@ const divide = (a,b) => {return a/b}
 const numberA = document.querySelector('#numberA')
 const operator = document.querySelector('#operator')
 const numberB = document.querySelector('#numberB')
-let numberAText = numberA.textContent
-let operatorText = operator.textContent
-let numberBText = numberB.textContent
+
 
 const operate = function(a,operating,b){
+    if (operating === '/' && (a === 0 || b ===0)) return alert('not allowed')
     switch(operating) {
         case '+':
             return add(a,b);
         case '-':
             return subtract(a,b);
         case '/':
-            return divide(a,b);
+            return divide(a,b).toFixed(2);
         case 'x':
-            return multiply(a,b)
+            return multiply(a,b).toFixed(2)
         case '=':
             try {return numberA.textContent = operate(numberA.textContent, operator.textContent, numberB.textContent)}
             catch(err){numberA.textContent=b; numberB.textContent=''; operator.textContent=''}
@@ -31,12 +30,19 @@ const operate = function(a,operating,b){
 
 const findButtonNumbers = document.querySelectorAll('#buttonList button')
 findButtonNumbers.forEach(item =>{item.addEventListener('click', function(){
-    const value = item.textContent
-    if (value === '+/-' && numberB.textContent) numberB.textContent = makeNegative(numberB.textContent);
-    else if(value === '+/-') return;
-    else if (value ==='Clear'){numberB.textContent = ''; numberA.textContent = ''; operator.textContent = '' } 
-    else if (operator.textContent === '') {numberA.textContent=''; numberB.textContent+=value;}
-    else numberB.textContent += value
+    const value = item.textContent;
+    if (value === '+/-' && numberB.textContent) {
+        numberB.textContent = makeNegative(numberB.textContent);
+        }else if(value === '+/-') {return;} 
+    if (value ==='Clear'){
+        if (numberB.textContent) {return numberB.textContent = '';}
+        else if(numberA.textContent || operator.textContent) {
+                numberA.textContent = ''; 
+                operator.textContent = '';
+                return;
+            }; 
+        };  
+    if (value != 'Clear' && value != '+/-') numberB.textContent += value;
 })})
     
 
@@ -48,9 +54,12 @@ findOperatorButtons.forEach(button =>{button.addEventListener('click', function(
         else if (numberA.textContent === ''){numberA.textContent = numberB.textContent; 
             numberB.textContent =''; operator.textContent = button.textContent}
         else if((numberA.textContent && operator.textContent && numberB.textContent)) {
-            numberA.textContent = operate(numberA.textContent, operator.textContent, numberB.textContent);
-            numberB.textContent = ''; 
-            !(button.textContent === '=')? operator.textContent = button.textContent: operator.textContent = '';
+            const value = operate(numberA.textContent, operator.textContent, numberB.textContent);
+            if (!(typeof value === 'Number') && value < Infinity){
+                numberA.textContent = value
+                numberB.textContent = ''; 
+                !(button.textContent === '=')? operator.textContent = button.textContent: operator.textContent = '';        
+            } else {alert('no.')};
         } else if (numberA.textContent && !numberB.textContent){operator.textContent = button.textContent}
     })})       
 
